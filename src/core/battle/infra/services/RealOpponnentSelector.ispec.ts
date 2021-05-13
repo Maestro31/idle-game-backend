ddff83d4e6a959643484a7263e5fbb08f9be3955
Emmanuel RICHE <emmanuel.riche@mysofie.fr>
2021-05-13 10:40:27 +0200

    fix commented tests

--- src/core/battle/infra/services/RealOpponnentSelector.ispec.ts
+++ src/core/battle/infra/services/RealOpponnentSelector.ispec.ts
@@ -35,76 +35,76 @@ describe('Opponent Selector', () => {
     await BattleResultDAO.destroy({ truncate: true })
   })
 
-  // it('should retrieve the only fighter that exists', async () => {
-  //   const expectedId = await createCharacterWithProps({ rank: 0 })
-  //   expect((await opponentSelector.to(fighter)).id).toBe(expectedId)
-  // })
+  it('should retrieve the only fighter that exists', async () => {
+    const expectedId = await createCharacterWithProps({ rank: 0 })
+    expect((await opponentSelector.to(fighter)).id).toBe(expectedId)
+  })
 
-  // it('should retrieve the character with the same rank', async () => {
-  //   await createCharacterWithProps({ rank: 9 })
-  //   const expectedId = await createCharacterWithProps({ rank: 10 })
-  //   expect((await opponentSelector.to(fighter)).id).toBe(expectedId)
-  // })
+  it('should retrieve the character with the same rank', async () => {
+    await createCharacterWithProps({ rank: 9 })
+    const expectedId = await createCharacterWithProps({ rank: 10 })
+    expect((await opponentSelector.to(fighter)).id).toBe(expectedId)
+  })
 
-  // it('should retrieve the character with the closest rank', async () => {
-  //   await createCharacterWithProps({ rank: 8 })
-  //   const expectedId = await createCharacterWithProps({ rank: 11 })
-  //   expect((await opponentSelector.to(fighter)).id).toBe(expectedId)
-  // })
+  it('should retrieve the character with the closest rank', async () => {
+    await createCharacterWithProps({ rank: 8 })
+    const expectedId = await createCharacterWithProps({ rank: 11 })
+    expect((await opponentSelector.to(fighter)).id).toBe(expectedId)
+  })
 
-  // it('should retrieve the character with the closest rank and the least amount of combat', async () => {
-  //   await createCharacterWithProps({ rank: 8 })
-  //   const expectedId = await createCharacterWithProps({ rank: 11 })
-  //   const id1 = await createCharacterWithProps({ rank: 11 })
-  //   const id2 = await createCharacterWithProps({ rank: 11 })
+  it('should retrieve the character with the closest rank and the least amount of combat', async () => {
+    await createCharacterWithProps({ rank: 8 })
+    const expectedId = await createCharacterWithProps({ rank: 11 })
+    const id1 = await createCharacterWithProps({ rank: 11 })
+    const id2 = await createCharacterWithProps({ rank: 11 })
 
-  //   await BattleResultDAO.create({
-  //     winnerID: id1,
-  //     looserID: uniqueIdAdapter.generate(),
-  //   })
-  //   await BattleResultDAO.create({
-  //     winnerID: uniqueIdAdapter.generate(),
-  //     looserID: id1,
-  //   })
-  //   await BattleResultDAO.create({
-  //     winnerID: uniqueIdAdapter.generate(),
-  //     looserID: id2,
-  //   })
+    await BattleResultDAO.create({
+      winnerID: id1,
+      looserID: uniqueIdAdapter.generate(),
+    })
+    await BattleResultDAO.create({
+      winnerID: uniqueIdAdapter.generate(),
+      looserID: id1,
+    })
+    await BattleResultDAO.create({
+      winnerID: uniqueIdAdapter.generate(),
+      looserID: id2,
+    })
 
-  //   expect((await opponentSelector.to(fighter)).id).toBe(expectedId)
-  // })
+    expect((await opponentSelector.to(fighter)).id).toBe(expectedId)
+  })
 
-  // it('should retrieve a random character when several match', async () => {
-  //   randomService.willRespond(2)
-  //   await createCharacterWithProps({ rank: 11 })
-  //   await createCharacterWithProps({ rank: 11 })
-  //   const expectedId = await createCharacterWithProps({ rank: 11 })
+  it('should retrieve a random character when several match', async () => {
+    randomService.willRespond(2)
+    await createCharacterWithProps({ rank: 11 })
+    await createCharacterWithProps({ rank: 11 })
+    const expectedId = await createCharacterWithProps({ rank: 11 })
 
-  //   expect((await opponentSelector.to(fighter)).id).toBe(expectedId)
-  // })
+    expect((await opponentSelector.to(fighter)).id).toBe(expectedId)
+  })
 
-  // it('should not retrieve a character with the same owner that the fighter', async () => {
-  //   await createCharacterWithProps({ rank: 10, ownerID: 'uuid-player-1' })
-  //   const expectedId = await createCharacterWithProps({
-  //     rank: 10,
-  //     ownerID: 'uuid-player-2',
-  //   })
+  it('should not retrieve a character with the same owner that the fighter', async () => {
+    await createCharacterWithProps({ rank: 10, ownerID: 'uuid-player-1' })
+    const expectedId = await createCharacterWithProps({
+      rank: 10,
+      ownerID: 'uuid-player-2',
+    })
 
-  //   expect((await opponentSelector.to(fighter)).id).toBe(expectedId)
-  // })
+    expect((await opponentSelector.to(fighter)).id).toBe(expectedId)
+  })
 
-  // it('should throwing an error if no one character is available', async () => {
-  //   await expect(opponentSelector.to(fighter)).rejects.toThrowError(
-  //     NoOpponentFoundException
-  //   )
-  // })
+  it('should throwing an error if no one character is available', async () => {
+    await expect(opponentSelector.to(fighter)).rejects.toThrowError(
+      NoOpponentFoundException
+    )
+  })
 
   it('should not retrieve a character who have a recovered time cooldown', async () => {
     await createCharacterWithProps({
       rank: 10,
       recoveredAt: new Date('2021-05-13 11:00:00'),
     })
-    console.log(await CharacterDAO.findAll())
+
     await expect(opponentSelector.to(fighter)).rejects.toThrowError(
       NoOpponentFoundException
     )
